98dd74ba9df014270e39db1c49bfcc6a023adf75
Guilherme Luz <>
2025-01-12 20:37:49 -0300

    feat: create application in spring and config

---
+++ src/test/java/br/fiap/schepis/hackton/HacktonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.schepis.hackton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacktonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
