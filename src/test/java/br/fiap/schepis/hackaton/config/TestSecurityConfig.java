fa892d92a104aade030cca37ce95e701214cfdcd
Dailan Bueno <daylansantos@gmail.com>
2025-02-06 08:35:20 -0300

    test keycloak endpoints

---
+++ src/test/java/br/fiap/schepis/hackaton/config/TestSecurityConfig.java
@@ -0,0 +1,25 @@
+package br.fiap.schepis.hackaton.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class TestSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(auth -> auth
+        .anyRequest().permitAll())
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Se for API stateless
+
+    return http.build();
+  }
+}
